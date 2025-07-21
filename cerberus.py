a74622e3f02af6eb133a4e70c95369bb417714b2
Phil Ware <philw@smithmyers.com>
2025-07-21 15:11:58 +0100

    Added more group cmds

--- cerberus.py
+++ cerberus.py
@@ -13,16 +13,79 @@ from testRunner import TestRunner
 
 def displayPlugins():
     displayPluginCategory("Equipment", manager.equipPlugins)
-    displayPluginCategory("Product", manager.productsPlugins)
+    displayPluginCategory("Product", manager.productPlugins)
     displayPluginCategory("Test", manager.testPlugins)
 
 
 def displayPluginCategory(category_name, plugins):
     logging.info(f"Available {category_name} plugins:")
-    for plugin in plugins.values():
-        logging.info(f" - {plugin.name}")
+    for name, plugin in plugins.items():
+        logging.info(f" - {name}: '{plugin.name}'")
 
     logging.info("")
+
+
+class EquipShell(cmd.Cmd):
+    intro = "Welcome to Cerberus Equipment System. Type help or ? to list commands.\n"
+    prompt = 'Equipment> '
+
+    def do_exit(self, arg):
+        """Exit the Cerberus Equipment shell"""
+        return True
+
+    def do_list(self, arg):
+        """List all of the Equipment"""
+        displayPluginCategory("Equipment", manager.equipPlugins)
+
+
+class ProductShell(cmd.Cmd):
+    intro = "Welcome to Cerberus Product System. Type help or ? to list commands.\n"
+    prompt = 'Product> '
+
+    def do_exit(self, arg):
+        """Exit the Cerberus Product shell"""
+        return True
+
+    def do_list(self, arg):
+        """List all of the Products"""
+        displayPluginCategory("Product", manager.productPlugins)
+
+
+class TestShell(cmd.Cmd):
+    def __init__(self, test):
+        TestShell.intro = f"Welcome to Cerberus {test.name} Test System. Type help or ? to list commands.\n"
+        TestShell.prompt = f"{test.name}> "
+
+        super().__init__()
+        self.test = test
+
+    def do_exit(self, arg):
+        """Exit the Cerberus {test.name} shell"""
+        return True
+
+    def do_run(self, arg):
+        testRunner.runTest(self.test)
+
+
+class TestsShell(cmd.Cmd):
+    intro = "Welcome to Cerberus Test System. Type help or ? to list commands.\n"
+    prompt = 'Tests> '
+
+    def do_exit(self, arg):
+        """Exit the Cerberus Test shell"""
+        return True
+
+    def do_list(self, arg):
+        """List all of the Tests"""
+        displayPluginCategory("Test", manager.testPlugins)
+
+    def do_load(self, testName):
+        """Loads a test"""
+        try:
+            test = manager.testPlugins[testName]
+            TestShell(test).cmdloop()
+        except KeyError:
+            print(f"Unknown test: {testName}")
 
 
 class Shell(cmd.Cmd):
@@ -34,24 +97,17 @@ class Shell(cmd.Cmd):
         print("Goodbye")
         exit(0)
 
-    def do_exit(self, arg):
-        """Exit Cerberus shell and exit the application"""
-        self.do_quit(arg)
+    def do_equipment(self, arg):
+        """Go into the Equipment shell subsystem"""
+        EquipShell().cmdloop()
 
-    def do_list(self, arg):
-        """List all of the supported Equipment, Products and Tests"""
-        displayPlugins()
+    def do_products(self, arg):
+        """Go into the Product shell subsystem"""
+        ProductShell().cmdloop()
 
-    def do_run(self, testName):
-        """Runs a test, after checking the requirements are valid"""
-        self.runTest(testName)
-
-    def runTest(self, testName):
-        try:
-            test = manager.testPlugins[testName]
-            testRunner.runTest(test)
-        except KeyError as e:
-            print(f"Unknown test: {testName}")
+    def do_tests(self, arg):
+        """Go into the Test shell subsystem"""
+        TestsShell().cmdloop()
 
 
 if __name__ == "__main__":
