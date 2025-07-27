0f4740e36ec93ab80d2d40cf65e1ac177722a584
PhilWare <phil@chanware.co.uk>
2025-07-27 04:29:20 +0100

    Silly index bug

--- cmdShells/common.py
+++ cmdShells/common.py
@@ -2,16 +2,10 @@ from typing import Dict
 from plugins.basePlugin import BasePlugin
 from testManager import TestManager
 
-def displayPlugins(manager: TestManager):
-    displayPluginCategory("Equipment", manager.equipment)
-    displayPluginCategory("Product", manager.products)
-    displayPluginCategory("Test", manager.tests)
-
-
 def displayPluginCategory(category_name, plugins: Dict[str, BasePlugin]):
     print(f"Available {category_name} plugins:")
     idx = 0
-    for _, plugin in plugins.items():
+    for plugin in list(plugins.values()):
         print(f" #{idx}: '{plugin.name}'")
         idx += 1
 
