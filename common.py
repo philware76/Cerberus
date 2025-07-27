3c7ce932f513ca7e57e415a9cfb5c0b4e7e243b4
Phil Ware <philw@smithmyers.com>
2025-07-27 12:13:35 +0100

    Got the collasible group box to auto-gen class methods

--- common.py
+++ common.py
@@ -1,3 +1,5 @@
+
+import re
 import time
 from threading import Event
 
@@ -30,3 +32,9 @@ def dwellEvent(period: float, stopEvent: Event = None):
         time.sleep(0.2)
         if stopEvent.wait(0.1):
             break
+
+
+def camel2Human(name: str) -> str:
+    # Add spaces before uppercase letters and capitalize the first letter
+    human_name = re.sub(r'([a-z])([A-Z])', r'\1 \2', name).capitalize()
+    return human_name
