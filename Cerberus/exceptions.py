585115c17c666b20c3f2803f4dc8115b1fe76112
PhilWare <phil@chanware.co.uk>
2025-08-04 11:48:51 +0200

    Added more exceptions/errors

--- Cerberus/exceptions.py
+++ Cerberus/exceptions.py
@@ -1,4 +1,22 @@
-class TestError(Exception):
+class CerberusException(Exception):
+    """Base class for all Cerberus exceptions."""
+    def __init__(self, message: str, *args, **kwargs):
+        super().__init__(message, *args)
+        self.message = message
+
+    def __str__(self):
+        return f"CerberusException: {self.message}"
+
+class PluginError(CerberusException):
+    """Custom exception raised for errors related to plugins in Cerberus."""
+    def __init__(self, message: str, *args, **kwargs):
+        super().__init__(message, *args)
+        self.message = message
+
+    def __str__(self):
+        return f"Cerberus.PluginError: {self.message}"
+
+class TestError(PluginError):
     """Custom exception raised for errors during test execution in Cerberus."""
     def __init__(self, message: str, *args, **kwargs):
         super().__init__(message, *args)
@@ -6,3 +24,21 @@ class TestError(Exception):
 
     def __str__(self):
         return f"Cerberus.TestError: {self.message}"
+        
+class EquipmentError(PluginError):
+    """Custom exception raised for errors related to equipment plugins in Cerberus."""
+    def __init__(self, message: str, *args, **kwargs):
+        super().__init__(message, *args)
+        self.message = message
+
+    def __str__(self):
+        return f"Cerberus.EquipmentError: {self.message}"
+
+class ExecutionError(CerberusException):
+    """Custom exception raised for errors during test execution in Cerberus."""
+    def __init__(self, message: str, *args, **kwargs):
+        super().__init__(message, *args)
+        self.message = message
+
+    def __str__(self):
+        return f"Cerberus.ExecutionError: {self.message}"
