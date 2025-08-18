803899874d04c5f73ec1077755a2c8693fa0af8a
Phil Ware <philw@smithmyers.com>
2025-08-18 01:00:51 +0100

    Added image start image

--- CerberusCLI.py
+++ CerberusCLI.py
@@ -1,6 +1,9 @@
 import sys
 
 from Cerberus.cmdShells.mainShell import runShell
+from show_image import displayImage
 
 if __name__ == "__main__":
+    displayImage(["Image1.png"])
+    print("SmithMeyers - 2025")
     runShell(sys.argv)
